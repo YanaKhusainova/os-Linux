76e59f766befd420c5690610a5c4c79f812bce90
YanaKhusainova <63101910+YanaKhusainova@users.noreply.github.com>
2020-09-19 12:32:57 +0300

    Create 1_2.sh

---
+++ 1.2/1_2.sh
@@ -0,0 +1,11 @@
+$ gcc -o fred.c bill.c
+$ ls *.o
+$ gcc -c program.c
+$ gcc -o program program.o bill.o
+$ ./program
+$ ar crv libfoo.a fred.o bill.o
+$ ranlib libfoo.a
+$ gcc -o program program.o libfoo.a
+$ ./program
+$ gcc -o program program.o -L. -lfoo
+$ ./program
